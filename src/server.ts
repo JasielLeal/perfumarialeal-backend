884e7558b46cf009d01a008bef72228b8b122548
Jasiel <jasieloficial@hotmail.com>
2024-05-13 09:21:28 -0300

    Fix

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,20 @@ import { routes } from "./routes";
 
 const app = express();
 
+const allowedOrigins = [process.env.FRONTEND, "http://localhost:5173"];
+
+console.log(allowedOrigins)
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
