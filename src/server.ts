cba6f1542c09a2a8122609bb293a400726f6642c
Jasiel <jasieloficial@hotmail.com>
2024-05-13 09:40:08 -0300

    Fixações

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { routes } from "./routes";
+import path from "path";
 
 const app = express();
 
-
+app.use(express.static(path.join(__dirname, 'https://perfumarialeal-front.vercel.app')));
 
 app.use(cors());
 app.use(express.json());
